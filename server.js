4b3422158cb467f337177de448fe13f8ddce65fa
THESHMIL0 <loolk448@gmail.com>
2026-02-23 21:44:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
-// Setup Express and our HTTP server
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files (like our HTML and client JS) from a folder called 'public'
 app.use(express.static('public'));
 
-// Store our players here
 const players = {};
 
-// Listen for connections from clients
 io.on('connection', (socket) => {
     console.log(`A player connected: ${socket.id}`);
 
-    // Add the new player to our state
+    // Add new player to the center of the screen
     players[socket.id] = {
-        x: 100, // starting X position
-        y: 100, // starting Y position
+        x: 400, 
+        y: 300, 
         health: 100
     };
 
-    // Send the current list of players to the newly connected player
     socket.emit('currentPlayers', players);
-
-    // Tell all *other* players that a new player has joined
     socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
 
-    // Handle a player disconnecting
+    // NEW: Listen for movement from a player
+    socket.on('playerMovement', (movementData) => {
+        // Update the server's record of this player
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        
+        // Tell all OTHER players about this movement
+        socket.broadcast.emit('playerMoved', { id: socket.id, x: movementData.x, y: movementData.y });
+    });
+
     socket.on('disconnect', () => {
         console.log(`Player disconnected: ${socket.id}`);
         delete players[socket.id];
-        // Tell everyone else this player left
         io.emit('playerDisconnected', socket.id);
     });
 });
 
-// Start the server on port 3000 (or the port Render assigns)
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
