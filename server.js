444d0ca73b0bf0327d1c3d13fe932991ffa6a6c7
THESHMIL0 <loolk448@gmail.com>
2026-02-24 11:54:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve the 'public' folder to the internet
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('A player connected:', socket.id);
 
-    // Bounce movement data to the other player
-    socket.on('syncMovement', (data) => {
-        socket.broadcast.emit('teammateMoved', data);
-    });
-
-    // Bounce laser data to the other player
-    socket.on('shoot', (projectile) => {
-        socket.broadcast.emit('teammateShot', projectile);
-    });
+    socket.on('syncMovement', data => socket.broadcast.emit('teammateMoved', data));
+    socket.on('shoot', proj => socket.broadcast.emit('teammateShot', proj));
+    
+    // NEW: Player 1 sends the official enemy list, server bounces it to Player 2
+    socket.on('hostSync', data => socket.broadcast.emit('hostSync', data));
+    
+    // NEW: Player 2 tells Player 1 they shot an enemy
+    socket.on('enemyHit', id => socket.broadcast.emit('enemyHit', id));
+    
+    // NEW: Share the Game Over screen
+    socket.on('gameOver', data => socket.broadcast.emit('gameOver', data));
 
     socket.on('disconnect', () => {
         console.log('A player disconnected');
@@ -28,6 +29,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
