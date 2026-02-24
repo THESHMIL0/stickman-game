9676b0698acafd0fde8d3ee6c2594dc4cebd090f
THESHMIL0 <loolk448@gmail.com>
2026-02-24 11:59:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Serve the 'public' folder
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
@@ -14,13 +15,13 @@ io.on('connection', (socket) => {
     socket.on('syncMovement', data => socket.broadcast.emit('teammateMoved', data));
     socket.on('shoot', proj => socket.broadcast.emit('teammateShot', proj));
     
-    // NEW: Player 1 sends the official enemy list, server bounces it to Player 2
+    // Player 1 sends the official enemy list, server bounces it to Player 2
     socket.on('hostSync', data => socket.broadcast.emit('hostSync', data));
     
-    // NEW: Player 2 tells Player 1 they shot an enemy
+    // Player 2 tells Player 1 they shot an enemy
     socket.on('enemyHit', id => socket.broadcast.emit('enemyHit', id));
     
-    // NEW: Share the Game Over screen
+    // Share the Game Over screen
     socket.on('gameOver', data => socket.broadcast.emit('gameOver', data));
 
     socket.on('disconnect', () => {
