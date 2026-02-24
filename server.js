b3fcdedd639e5beb3e8b3c7b585a884582f36eb5
THESHMIL0 <loolk448@gmail.com>
2026-02-24 11:35:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,18 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Tell the server to serve your 'public' folder
+// Serve the 'public' folder to the internet
 app.use(express.static('public'));
 
-// When a player connects...
 io.on('connection', (socket) => {
     console.log('A player connected:', socket.id);
 
-    // When Player 1 or 2 moves, send their data to everyone else
+    // Bounce movement data to the other player
     socket.on('syncMovement', (data) => {
         socket.broadcast.emit('teammateMoved', data);
     });
 
-    // When a player shoots, send the bullet to the other player
+    // Bounce laser data to the other player
     socket.on('shoot', (projectile) => {
         socket.broadcast.emit('teammateShot', projectile);
     });
